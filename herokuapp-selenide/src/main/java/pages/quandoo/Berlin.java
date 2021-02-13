50d529b89b27e52cc8c8875df7b64f807f891bab
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-13 18:58:16 +0100

    quanduu.de review test update 1.test

--- herokuapp-selenide/src/main/java/pages/quandoo/Berlin.java
+++ herokuapp-selenide/src/main/java/pages/quandoo/Berlin.java
@@ -1,6 +1,5 @@
 package pages.quandoo;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -8,18 +7,30 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class Berlin {
 
+    @FindBy(id = "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+    private SelenideElement cookiesOkBtn;
+
     @FindBy(xpath = "//button[@data-qa='filter-button-top-rated']")
-    private SelenideElement topRated;
+    private SelenideElement topRatedBtn;
 
     @FindBy(xpath = "//div[@id='desktop-filters-wrapper']//span[contains(text(), 'Review score')]")
-    private SelenideElement reviewScore;
+    private SelenideElement topRatedFilter;
 
-    public void topRatedClick(){
-        $(topRated).click();
+    public void acceptCookies() {
+        cookiesOkBtn.click();
     }
-     public SelenideElement isFilterVisible(){
-        return reviewScore;
 
-     }
+    public void topRatedClick() {
+        topRatedBtn.click();
+    }
+
+    public SelenideElement topRatedFilter() {
+        return topRatedFilter;
+
+    }
+
+    public SelenideElement isActive() {
+        return topRatedBtn;
+    }
 
 }
--- herokuapp-selenide/src/test/java/quandoo/BerlinTest.java
+++ herokuapp-selenide/src/test/java/quandoo/BerlinTest.java
@@ -1,8 +1,9 @@
 package quandoo;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.open;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pages.quandoo.Berlin;
 
@@ -10,11 +11,20 @@ public class BerlinTest {
 
     Berlin berlin;
     String reviewText = "Review score: 4-6";
+    String url = "https://www.quandoo.de/en/berlin";
+
+    @BeforeEach
+    public void openPage(){
+        berlin = open(url, Berlin.class);
+        berlin.acceptCookies();
+    }
 
     @Test
     public void reviewTest() {
-        berlin = open("https://www.quandoo.de/en/berlin", Berlin.class);
+
         berlin.topRatedClick();
-        berlin.isFilterVisible().shouldHave(text(reviewText));
+        berlin.topRatedFilter().should(exist);
+        berlin.topRatedFilter().shouldHave(text(reviewText));
+       berlin.isActive().shouldHave(attribute("data-state", "active"));
     }
 }
