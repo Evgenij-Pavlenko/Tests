6355e7a50cc4cefb0bf630b9625edbd26ee0dc82
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-17 18:15:03 +0100

    search assert

--- idealo2/src/main/java/com/example/idealo2/Search.java
+++ idealo2/src/main/java/com/example/idealo2/Search.java
@@ -1,8 +1,14 @@
 package com.example.idealo2;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.switchTo;
 
 public class Search {
 
@@ -12,18 +18,33 @@ public class Search {
 //    никак не находит. И задержка не работает
 
 //    @FindBy(css = "btn-accept-all")
-    @FindBy(xpath = "/html/body/div/div[3]/div[4]/div[2]/button")
-    private SelenideElement cookiesBtn;
+//    @FindBy(xpath = "/html/body/div/div[3]/div[4]/div[2]/button")
+//    private SelenideElement cookiesBtn;
 
     @FindBy(id = "i-search-input")
     private SelenideElement searchInput;
 
-    public void cookiesAccept(){
-        cookiesBtn.click();
+    @FindBy(className = "offerList-title")
+    private SelenideElement searchedText;
+
+    WebDriverRunner wr = new WebDriverRunner();
+
+    By okBtnCookies = By.cssSelector(".btn-accept-all");
+    By iframe = By.cssSelector("body > div > iframe");
+    WebElement frame = switchTo().frame($(iframe)).findElement(By.cssSelector("body"));
+
+    public void acceptCookies() {
+        $(frame).$(okBtnCookies).click();
+        WebDriverRunner.getWebDriver().switchTo().parentFrame();
     }
 
-    public void search(String name){
+    public void search(String name) {
         searchInput.setValue(name);
         searchInput.sendKeys(Keys.ENTER);
+
+    }
+
+    public SelenideElement searchText(){
+        return searchedText;
     }
 }
--- idealo2/src/test/java/com/example/idealo2/SearchTest.java
+++ idealo2/src/test/java/com/example/idealo2/SearchTest.java
@@ -1,5 +1,6 @@
 package com.example.idealo2;
 
+import com.codeborne.selenide.Condition;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -7,16 +8,18 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTest {
     Search search;
-    private String url ="https://www.idealo.de/";
+    private String url = "https://www.idealo.de/";
+    String searchedText = "E-Bikes";
 
     @BeforeTest
-    public void before(){
-
-    }
-    @Test
-    public void searchTest(){
+    public void before() {
         search = open("https://www.idealo.de/", Search.class);
-        search.cookiesAccept();
-        search.search("E-Bikes");
+        search.acceptCookies();
+    }
+
+    @Test
+    public void searchTest() {
+        search.search(searchedText);
+        search.searchText().shouldHave(Condition.text(searchedText));
     }
 }
