40612e5b92d094642f9e306e4021db3eaff3a8a3
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-02-17 01:19:15 +0100

    idealo.de

---
+++ idealo2/src/test/java/com/example/idealo2/SearchTest.java
@@ -0,0 +1,22 @@
+package com.example.idealo2;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTest {
+    Search search;
+    private String url ="https://www.idealo.de/";
+
+    @BeforeTest
+    public void before(){
+
+    }
+    @Test
+    public void searchTest(){
+        search = open("https://www.idealo.de/", Search.class);
+        search.cookiesAccept();
+        search.search("E-Bikes");
+    }
+}
